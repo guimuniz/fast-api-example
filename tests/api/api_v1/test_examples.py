c8ff43062aa388e63a1bfb7a5c791221611b61a8
guimuniz <guisp1901@gmail.com>
2022-08-10 09:42:26 -0300

    fix test

--- tests/api/api_v1/test_examples.py
+++ tests/api/api_v1/test_examples.py
@@ -16,14 +16,14 @@ def examples_data():
 
 
 def test_get_examples_success(mocker, client, examples_data):
-    mock_assets = mocker.patch('app.client.client.Client.root', return_value=examples_data)
+    mock_examples = mocker.patch('app.client.client.Client.root', return_value=examples_data)
     response = client.get(
         url="/api/v1/examples/", headers={"API_KEY": settings.API_KEY}
     )
 
-    mock_assets.assert_called_once()
+    mock_examples.assert_called_once()
     assert response.status_code == 200
-    first_asset = response.json()["assets"][0]
-    assert len(first_asset) == 2
-    assert first_asset["name"] == "Forta"
-    assert first_asset["symbol"] == "fort"
+    first_example = response.json()[0]
+    assert len(first_example) == 2
+    assert first_example["name"] == "Forta"
+    assert first_example["symbol"] == "fort"
